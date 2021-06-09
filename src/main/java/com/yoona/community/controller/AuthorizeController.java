6be04a6a7edef23b50c5e789e3ee645a75a7026d
z l <454170054@qq.com>
2021-06-09 19:49:25 +0800

    update oAuth

---
+++ src/main/java/com/yoona/community/controller/AuthorizeController.java
@@ -0,0 +1,40 @@
+package com.yoona.community.controller;
+
+
+import com.yoona.community.dto.AccessTokenDTO;
+import com.yoona.community.dto.GithubUser;
+import com.yoona.community.provider.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GitHubProvider gitHubProvider;
+
+    @Value("${github.client.id}")
+    private String client_id;
+
+    @Value("${github.client.secret}")
+    private String client_secret;
+
+    @Value("${github.redirect.uri}")
+    private String redirect_uri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam("code") String code){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id(client_id);
+        accessTokenDTO.setClient_secret(client_secret);
+        accessTokenDTO.setRedirect_uri(redirect_uri);
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = gitHubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
