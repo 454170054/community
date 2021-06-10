35413d3905576a41edb467575e539748b5db9e26
z l <454170054@qq.com>
2021-06-10 20:54:05 +0800

    add flyway

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,14 @@
+create table USER
+(
+    ID           INT auto_increment,
+    ACCOUNT_ID   VARCHAR(100),
+    NAME         VARCHAR(50),
+    TOKEN        CHAR(36),
+    GMT_CREATE   BIGINT,
+    GMT_MODIFIED BIGINT,
+    constraint USER_PK
+        primary key (ID)
+);
+
+create unique index USER_ID_UINDEX
+    on USER (ID);
