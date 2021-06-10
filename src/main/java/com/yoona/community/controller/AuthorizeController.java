edfe51e57c891839781d3893e0cb092c2c39af8c
z l <454170054@qq.com>
2021-06-10 20:10:32 +0800

    add cookies

--- src/main/java/com/yoona/community/controller/AuthorizeController.java
+++ src/main/java/com/yoona/community/controller/AuthorizeController.java
@@ -12,7 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -34,7 +35,7 @@ public class AuthorizeController {
     private UserMapper userMapper;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam("code") String code, HttpServletRequest request){
+    public String callback(@RequestParam("code") String code, HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(client_id);
@@ -44,14 +45,15 @@ public class AuthorizeController {
         GithubUser githubUser = gitHubProvider.getUser(accessToken);
         if(accessToken != null && githubUser != null){
             // 登陆成功，写cookie和session
+            String token = UUID.randomUUID().toString();
             User user = new User();
             user.setName(githubUser.getName());
-            user.setToken(UUID.randomUUID().toString());
+            user.setToken(token);
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            request.getSession().setAttribute("user", githubUser);
+            response.addCookie(new Cookie("token", token));
             return "redirect:/";
         }else {
             // 登陆失败
